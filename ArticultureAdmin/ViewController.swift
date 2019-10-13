3e34c90a70d073886580eab340c210f8d79bd7cb
Fateh Khan <fatehkhann@gmail.com>
2019-10-13 12:43:33 +0500

    Initial Commit

---
+++ ArticultureAdmin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ArticultureAdmin
+//
+//  Created by Zarrar Company on 13/10/2019.
+//  Copyright © 2019 Zarrar Company. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
